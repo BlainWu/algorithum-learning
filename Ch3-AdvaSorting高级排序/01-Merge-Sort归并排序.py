7b60bdeeb779d30f7839695933d7b10443bb3937
peilin <896511142@qq.com>
2020-09-26 11:04:02 +0800

    update

--- Ch3-AdvaSorting高级排序/01-Merge-Sort归并排序.py
+++ Ch3-AdvaSorting高级排序/01-Merge-Sort归并排序.py
@@ -8,35 +8,37 @@
 =================================================='''
 from utils import *
 
+@function_timer
 def merge_sort(arr):
-    __merge_sort(arr,0,len(arr)-1)
+    __merge_sort(arr, 0, len(arr) - 1)
 
-def __merge_sort(arr,l,r):
-    if l>=r :
+def __merge_sort(arr, l, r):
+    """递归使用归并排序，对arr[l...r]的范围进行排序"""
+    if r <= l :
         return
-    '''这里还可以把l>=r这部分换成插入排序当排序范围为小于15时）'''
-    mid = l + (r-l) // 2
-    __merge_sort(arr,l,mid)
-    __merge_sort(arr,mid+1,r)
-    if(arr[mid]>arr[mid+1]):
-        __merge(arr,l,mid,r)
+    mid = l + (r - l) // 2
+    __merge_sort(arr, l, mid)
+    __merge_sort(arr, mid + 1, r)
+    __merge(arr, l, mid, r)
 
-def __merge(arr,l,mid,r):
-    aux = [arr[i] for i in range(l,r+1)]
-    i , j = 1 , mid+1
-    for k in range(l,r+1):
-        if aux[i-1] < aux[j-1]:
-            arr[k] = aux[i-1]
-            i+=1
-        elif aux[i-1] >=aux[j-1]:
-            arr[l] = aux[j-1]
-            j+=1
-        elif i>mid:
-            arr[k] = aux[j-l]
-            j+=1
-        elif j>r:
-            arr[k] = aux[i-1]
-            i+=1
+def __merge(arr, l, mid, r):
+    """将arr[l...mid]和arr[mid+1...r]两部分进行归并"""
+    aux = [arr[i] for i in range(l, r + 1)]
+    i, j = l, mid + 1
+    for k in range(l, r + 1):
+        if i > mid:
+            arr[k] = aux[j - l]
+            j += 1
+        elif j > r:
+            arr[k] = aux[i - l]
+            i += 1
+        elif aux[i - l] < aux[j - l]:
+            arr[k] = aux[i - l]
+            i += 1
+        else:
+            arr[k] = aux[j - l]
+            j += 1
 
 if __name__ == '__main__':
-    pass
+    arr = generateRandomArray(3000,1,100)
+    merge_sort(arr)
--- Ch3-AdvaSorting高级排序/02-Quick-Sort快速排序.py
+++ Ch3-AdvaSorting高级排序/02-Quick-Sort快速排序.py
@@ -6,13 +6,76 @@
 @Date   ：2020/9/24 0:06
 @Desc   ：
 =================================================='''
-'''使用递归，所以用两个不同的函数'''
-def quickSort():
-    pass
+import random
+from utils import generateRandomArray,function_timer
 
-def __quickSort():
+
+'''使用递归，所以用两个不同的函数'''
+@function_timer
+def quickSort(arr):
+    __quickSort(arr,0,len(arr)-1)
+
+def __quickSort(arr,l,r):
+    if l>=r:
+        return
+    p = __partition(arr,l,r)
+    __quickSort(arr,l,p-1)
+    __quickSort(arr,p+1,r)
+
+def __partition(arr,l,r):
+    #random_index = random.randint(l,r)
+    #arr[l],arr[random_index] = arr[random_index],arr[l]
+    v,j = arr[l],l
+    for  i in range(l+1,len(arr)-1):
+        if arr[i] < v:
+            arr[i],arr[j+1] = arr[j+1],arr[i]
+            j+=1
+    arr[l],arr[j] = arr[j],arr[l]
+    return j
+
+@function_timer
+def quickSort_rand(arr):
+    __quickSort_rand(arr,0,len(arr)-1)
+
+def __quickSort_rand(arr,l,r):
+    if l>=r:
+        return
+    p = __partition_rand(arr,l,r)
+    __quickSort_rand(arr,l,p-1)
+    __quickSort_rand(arr,p+1,r)
+
+def __partition_rand(arr,l,r):
+    random_index = random.randint(l,r)
+    arr[l],arr[random_index] = arr[random_index],arr[l]
+    v = arr[l]
+    j=l
+    for  i in range(l+1,len(arr)-1):
+        if arr[i] < v:
+            arr[i],arr[j+1] = arr[j+1],arr[i]
+            j+=1
+    arr[l],arr[j] = arr[j],arr[l]
+    return j
+
+'''双路快排'''
+@function_timer
+def quickSort2(arr):
+    __quickSort2(arr,0,len(arr)-1)
+
+def __quickSort2(arr,l,r):
+    if l>=r:
+        return
+    p = __partition2(arr,l,r)
+    __quickSort2(arr,l,p-1)
+    __quickSort2(arr,p+1,r)
+
+def __partition2(arr,l,r):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    arr = generateRandomArray(3000,1,100)
+    arr1 = arr.copy()
+    arr2 = arr.copy()
+    quickSort(arr1)
+    quickSort_rand(arr2)
+    #print(arr)
